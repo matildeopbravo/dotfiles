f72a6382692fb1345bd00f4fae354c3c100513d1
matildeopbravo <matildeopbravo@gmail.com>
2020-09-20 01:38:11 +0100

    fixed the problem with empty line in installation script

--- installation/install-dotfiles.sh
+++ installation/install-dotfiles.sh
@@ -5,8 +5,6 @@ chrome_dir="$HOME/.mozilla/firefox/*default/chrome"
 
 main() {
 
-   install_packages
-   create_chrome_dir
    create_symlinks
 }
 
@@ -17,15 +15,15 @@ create_symlinks() {
 
    while read -r line ; do   
 
-     if [[ "$line" = dest_dir* ]]
-     then
+     if [[ "$line" = dest_dir* ]] ; then
         eval "$line" #dest_dir= directory where symlink will be created
-     else
-     link_name="$(basename "$line")"
-         rm -r "${dest_dir:?}/$link_name"
-         ln -sf "$dotfiles_dir/$line" "$dest_dir/$link_name"
-    fi
-    done < ""$dotfiles_dir"/installation/symlinks.txt"
+	echo "evaled this $line"
+     elif [[ "$line" != "" ]] ; then
+     	 link_name="$(basename "$line")"
+         echo "tryna rm this ${dest_dir:?}/$link_name"
+         echo "tryna link this$dotfiles_dir/$line" "$dest_dir/$link_name" 
+     fi
+     done < ""$dotfiles_dir"/installation/symlinks.txt"
 
 }
 
