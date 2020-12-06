b94aa847574563a24d31769a7e1b772fe44f31c3
Matide Bravo <matildeopbravo@gmail.com>
2020-12-06 22:24:03 +0000

    added fgrep function

--- configs/zsh/functions.zsh
+++ configs/zsh/functions.zsh
@@ -25,7 +25,19 @@ sendfile() {
   echo "http://pasok.xyz/files/$1" | xclip -selection c
 }
 
-
+rga-fzf() {
+	RG_PREFIX="rga --files-with-matches"
+	local file
+	file="$(
+		FZF_DEFAULT_COMMAND="$RG_PREFIX '$1'" \
+			fzf --sort --preview="[[ ! -z {} ]] && rga --pretty --context 5 {q} {}" \
+				--phony -q "$1" \
+				--bind "change:reload:$RG_PREFIX {q}" \
+				--preview-window="70%:wrap"
+	)" &&
+	echo "opening $file" &&
+	xdg-open "$file"
+}
 
 extract() {
   if [ -f "$1" ] ; then
