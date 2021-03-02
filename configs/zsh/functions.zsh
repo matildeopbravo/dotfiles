a592f4b7dc502b90ee53dd5c618db81dc9460177
Matide Bravo <matildeopbravo@gmail.com>
2021-03-02 22:04:53 +0000

    added zsh functions

--- configs/zsh/functions.zsh
+++ configs/zsh/functions.zsh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 function op () {
-    xdg-open "$@" & disown
+    for i in "$@"
+    do
+      xdg-open "$i" & disown
+done
 }
 
 function nospace() {
     for file in "$@"; do
-        mv -vn "$file" "$(echo "$file" | sed -r "s/['&,()!]//g;s/ - /-/g;s/ _ /_/g;s/ /_/g;s/__/_/g")"
+        /usr/bin/mv -vn "$file" "$(echo "$file" | sed -r "s/['&,()!]//g;s/ - /-/g;s/ _ /_/g;s/ /_/g;s/__/_/g")"
 done;
 
 }
